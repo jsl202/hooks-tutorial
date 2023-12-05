0e12cfebdfb33e24b8bf2cf16a1a750479bbc730
jsl202 <jsl202@naver.com>
2023-12-05 12:44:14 +0900

    Hooks_useReduce

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import { useState } from "react";
 import Info from "./08_Hooks/Info";
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  return <Info />;
 };
 
 export default App;
